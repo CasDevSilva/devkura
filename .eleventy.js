5222bf7512599d0ae7efdb5d9f5ee002789eacee
CasDevSilva <sca.dev.silva@gmail.com>
2025-04-13 14:07:28 -0500

    config: Changes configuration referenced styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,19 @@
 module.exports = function(eleventyConfig) {
 
+    const mObjOrder = {
+        "inicio": 0,
+        "javascript": 1,
+        "node": 2,
+        "eleventy(11ty)": 3,
+        "sql": 4,
+        "sql_lite": 5,
+        "computer_Science": 6,
+        "python": 7,
+        "git": 8,
+        "recursos": 9,
+        "otros": 10
+    }
+
     eleventyConfig.addCollection("notes", function (collectionApi) {
         const mArrFiles = collectionApi.getFilteredByGlob("notes/**/*.md")
         const mSubArr = [];
@@ -16,8 +30,23 @@ module.exports = function(eleventyConfig) {
             }
         });
 
-        return mSubArr;
+        let mArrOrder = [];
+        Object.keys(mObjOrder).forEach(mRowOrder => {
+            let mObjResponse = mSubArr.find(mRowTemp => mRowTemp.parent.template.fileSlugStr == (mRowOrder == "inicio" ? "":mRowOrder))
+
+            mObjResponse.children.sort((a, b) => {
+                const nameA = a.data.title?.toLowerCase() || "";
+                const nameB = b.data.title?.toLowerCase() || "";
+                return nameA.localeCompare(nameB);
+            });
+
+            mArrOrder[mObjOrder[mRowOrder]] = mObjResponse
+        })
+
+        return mArrOrder;
     })
+
+    eleventyConfig.addPassthroughCopy("styles")
 
     return {
         dir: {
